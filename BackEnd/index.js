72a7d19d90ebd56c235cd236c3fec1f39de7dee7
GUSTAVO SANTOS PAIXAO DE NARDI <CG3026701@car.ifsp>
2025-06-18 10:34:13 -0300

    Test form eventos

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const userRoutes = require('./src/routes/userRoutes');
 const eventoRoutes = require('./src/routes/eventoRoutes');
 
 const app = express();
+
+// Permitir requisições de outros domínios (útil para quando for conectar com front separado)
+const cors = require('cors');
+app.use(cors());
+
 app.use(bodyParser.json());
 
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rotas da API
 app.use(userRoutes);
 app.use(eventoRoutes);
+
+// Rota para carregar o formulário HTML no navegador
+app.get('/evento-form', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'evento.html'));
+});
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
+
 // http://localhost:3000
